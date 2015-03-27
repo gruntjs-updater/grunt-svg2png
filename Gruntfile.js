355514cea499a8c0039637c8e2e8cf50aff292eb
Michael <michael@soapmedia.co.uk>
2015-03-27 15:04:04 +0000

    Info update for publishing on NPM.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 /*!
- * grunt-svg2png
- * https://github.com/dbushell/grunt-svg2png
+ * grunt-svg2png-width
+ * https://github.com/emcarru/grunt-svg2png
  *
  * Copyright (c) 2013 David Bushell
  * Licensed under The MIT License (MIT)
